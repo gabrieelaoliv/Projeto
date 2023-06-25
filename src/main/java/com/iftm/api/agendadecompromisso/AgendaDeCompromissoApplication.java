26a1f763e0ffdbbd6fa58f81a805f80390f0a67d
gabrieelaoliv <gaby_oliveira156@hotmail.com>
2023-06-25 19:01:37 -0300

    first commit

---
+++ src/main/java/com/iftm/api/agendadecompromisso/AgendaDeCompromissoApplication.java
@@ -0,0 +1,13 @@
+package com.iftm.api.agendadecompromisso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDeCompromissoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDeCompromissoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iftm/api/agendadecompromisso/AgendaDeCompromissoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.api.agendadecompromisso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaDeCompromissoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
