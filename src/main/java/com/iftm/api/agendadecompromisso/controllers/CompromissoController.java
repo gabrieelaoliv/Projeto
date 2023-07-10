d4007bf37293233b10ce2018cf44309cbce19ed4
gabrieelaoliv <gaby_oliveira156@hotmail.com>
2023-07-10 00:59:49 -0300

    swagger

--- src/main/java/com/iftm/api/agendadecompromisso/controllers/CompromissoController.java
+++ src/main/java/com/iftm/api/agendadecompromisso/controllers/CompromissoController.java
@@ -1,7 +1,15 @@
 package com.iftm.api.agendadecompromisso.controllers;
 
 import com.iftm.api.agendadecompromisso.data.vo.CompromissoVO;
+import com.iftm.api.agendadecompromisso.data.vo.UsuarioVO;
 import com.iftm.api.agendadecompromisso.services.CompromissoService;
+import com.iftm.api.agendadecompromisso.utils.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,37 +17,131 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/compromisso")
+@Tag(name = "Compromisso", description = "Endpoint for managing users.")
 
 public class CompromissoController {
     @Autowired
     private CompromissoService compromissoService;
 
     @GetMapping
+    @Operation(
+            summary = "Find all compromissos.", description = "Find all compromissos.", tags = {"Compromisso"},
+            responses = {
+                    @ApiResponse(description = "Sucess.", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = MediaType.APPLICATION_JSON,
+                                            array = @ArraySchema(schema = @Schema(implementation = CompromissoVO.class)))
+                            }
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content),
+            })
     public List<CompromissoVO> findAll() {
         return compromissoService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Find a compromisso by ID.", description = "Find a compromisso by ID.", tags = {"Compromisso"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = MediaType.APPLICATION_JSON,
+                                            schema = @Schema(implementation = CompromissoVO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public CompromissoVO findById(@PathVariable("id") Long id) {
         return compromissoService.findById(id);
     }
 
+    //query
     @GetMapping("/titulo/{texto}")
+    @Operation(
+            summary = "Find a compromisso by titulo.", description = "Find a compromisso by titulo.",
+            tags = {"Compromisso"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = MediaType.APPLICATION_JSON,
+                                            schema = @Schema(implementation = CompromissoVO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public List<CompromissoVO> findByTitulo(@PathVariable("texto") String texto) {
         return compromissoService.findByTitulo(texto);
     }
 
     @PostMapping
+    @Operation(
+            summary = "Create a compromisso.", description = "Create a compromisso.", tags = {"Compromisso"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = CompromissoVO.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public CompromissoVO save(@RequestBody CompromissoVO compromissoVO) {
         return compromissoService.salvarCompromisso(compromissoVO);
     }
 
     @PutMapping
+    @Operation(
+            summary = "Update a compromisso.", description = "Update a compromisso.", tags = {"Compromisso"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = CompromissoVO.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public CompromissoVO update(@RequestBody CompromissoVO compromissoVO) {
         return compromissoService.update(compromissoVO);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Deletes a Compromisso by ID.", description = "Deletes a Compromisso by ID.",
+            tags = {"Compromisso"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public String delete(@PathVariable("id") Long id) {
         return compromissoService.delete(id);
     }
--- src/main/java/com/iftm/api/agendadecompromisso/controllers/UsuarioController.java
+++ src/main/java/com/iftm/api/agendadecompromisso/controllers/UsuarioController.java
@@ -1,38 +1,121 @@
 package com.iftm.api.agendadecompromisso.controllers;
+
 import com.iftm.api.agendadecompromisso.data.vo.UsuarioVO;
 import com.iftm.api.agendadecompromisso.services.UsuarioService;
+import com.iftm.api.agendadecompromisso.utils.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 @RequestMapping("/api/v1/usuario")
+@Tag(name = "Usuario", description = "Endpoint for managing users.")
 
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping
+    @Operation(
+            summary = "Find all users.", description = "Find all users.", tags = {"Usuario"},
+            responses = {
+                    @ApiResponse(description = "Sucess.", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = MediaType.APPLICATION_JSON,
+                                            array = @ArraySchema(schema = @Schema(implementation = UsuarioVO.class)))
+                            }
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content),
+            })
     public List<UsuarioVO> findAll() {
         return usuarioService.findAll();
     }
 
     @GetMapping ("/{id}")
+    @Operation(
+            summary = "Find a user by ID.", description = "Find a user by ID.", tags = {"Usuario"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(mediaType = MediaType.APPLICATION_JSON,
+                                            schema = @Schema(implementation = UsuarioVO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public UsuarioVO findById(@PathVariable("id") Long id) {
         return usuarioService.findById(id);
     }
 
     @PostMapping
+    @Operation(
+            summary = "Create a user.", description = "Create a user.", tags = {"Usuario"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = UsuarioVO.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public UsuarioVO save (@RequestBody UsuarioVO usuarioVO) {
         return usuarioService.salvarUsuario(usuarioVO);
     }
 
     @PutMapping
+    @Operation(
+            summary = "Update a user.", description = "Update a user.", tags = {"Usuario"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(
+                                    schema = @Schema(implementation = UsuarioVO.class)
+                            )
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public UsuarioVO update(@RequestBody UsuarioVO usuarioVO) {
         return usuarioService.update(usuarioVO);
     }
 
     @DeleteMapping ("/{id}")
+    @Operation(
+            summary = "Deletes a Usuario by ID.", description = "Deletes a Usuario by ID.",
+            tags = {"Usuario"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500",
+                            content = @Content)
+            }
+    )
     public String delete(@PathVariable("id") Long id) {
         return usuarioService.delete(id);
     }
